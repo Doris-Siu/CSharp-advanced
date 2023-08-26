4c54063775bd63aed686a55b8594640ac73c88f0
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-08-26 20:31:57 +0100

    creating an extension method to String instance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharp_advanced;
+using System;
+
+
+// Extensions
+string post = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+var shortenedPost = post.Shorten(5);
+
+Console.WriteLine(shortenedPost);
+
---
+++ StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CSharp_advanced
+{
+	public static class StringExtensions
+	{
+		// creating an extension method to String instance
+		public static string Shorten(this String str, int numberOfWords)
+		{
+			if (numberOfWords <= 0)
+				throw new ArgumentOutOfRangeException("the agrument should be greater than 0.");
+
+			var words = str.Split(' ');
+
+			if (words.Length <= numberOfWords)
+				return str;
+
+			return string.Join(' ', words.Take(numberOfWords)) + "...";
+
+		}
+		
+	}
+}
+
