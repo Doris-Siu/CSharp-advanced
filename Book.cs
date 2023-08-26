f740cf01dc5cf03bfbdb409a0d06d1b9e3e9666b
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-08-26 23:13:14 +0100

    LINQ - query operators & extension methods

---
+++ Book.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CSharp_advanced
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,3 +9,55 @@ var shortenedPost = post.Shorten(5);
 
 Console.WriteLine(shortenedPost);
 
+
+// LINQ
+var books = new BookRepository().GetBooks();
+
+//// LINQ Query Operators
+//var cheaperBooks =
+//    from b in books
+//    where b.Price < 10
+//    orderby b.Title
+//    select b.Title;
+
+
+// LINQ Extension Methods
+var cheaperBooks = books
+    .Where(b => b.Price < 10) // predicates -> lambda expressions
+    .OrderBy(b => b.Title)
+    .Select(b => b.Title);
+
+
+foreach (var book in cheaperBooks)
+{
+    Console.WriteLine(book);
+}
+
+// other useful extension methods
+books.Where(); // returns a list of matches
+books.Single();
+books.SingleOrDefault(); // return a single match or null
+
+books.First();
+books.FirstOrDefault();
+books.Last();
+books.LastOrDefault();
+
+// aggregate functions
+books.Min();
+books.Max();
+books.Count();
+books.Sum(b => b.Price);
+books.Average(b => b.Price);
+
+// used for paging
+books.Skip(3).Take(2);
+
+
+
+
+
+
+
+
+
